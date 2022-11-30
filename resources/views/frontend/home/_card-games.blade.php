fd8f6a60e47cda8c22a8629e67fcb07460a7c7c6
Rijal Ahmad Taufik <rijalahmadt@gmail.com>
2022-11-30 15:29:30 +0700

    Add home page frontend

---
+++ resources/views/frontend/home/_card-games.blade.php
@@ -0,0 +1,28 @@
+<div class="col-6 col-md-3 col-lg-4 col-xl-3 text-center px-lg-5">
+  <a href="#">
+    <div class="games__card">
+      <div class="games-card__body">
+        <img src="{{ asset('assets/website/images/games/fol-games-image.png') }}" alt="fight of legends">
+      </div>
+      <div class="games-card__footer">
+        <div class="games-card-footer__text">
+          Fight of Legends
+        </div>
+      </div>
+    </div>
+  </a>
+</div>
+<div class="col-6 col-md-3 col-lg-4 col-xl-3 px-lg-5">
+  <a href="#">
+    <div class="games__card">
+      <div class="games-card__body">
+        <img src="{{ asset('assets/website/images/logo/esi.png') }}" alt="fight of legends">
+      </div>
+      <div class="games-card__footer">
+        <div class="games-card-footer__text">
+          ESPORT STAR INDONESIA
+        </div>
+      </div>
+    </div>
+  </a>
+</div>
---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+  <nav class="header__navbar navbar navbar-expand-lg">
+    <div class="container-fluid container-md">
+      <a href="#" >
+        <div class="navbar-brand">
+          <img src="~/assets/images/logo/esi.png" alt="" height="40" class="d-inline-block align-text-center">
+          ESIGAMESHOP
+        </div>
+      </a>
+    </div>
+  </nav>
+</header>
