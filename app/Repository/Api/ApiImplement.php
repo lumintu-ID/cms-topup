950f2f263741bab3de80ebaddb8db0a485b59454
gumelarid <galuhprahadi96@gmail.com>
2022-12-12 10:12:43 +0700

    add parsing required data

--- app/Repository/Api/ApiImplement.php
+++ app/Repository/Api/ApiImplement.php
@@ -32,27 +32,66 @@ class ApiImplement implements ApiRepository
 
     public function priceList($payment, $gameId)
     {
+        $Id_GOC = "Esp5373790";
+        $Id_Unipin = "bcd3a3a3-4c68-419e-bd79-91d8678faf04";
+        $Id_GV = "70";
 
+        $notif_Unipin = url('/api/v1/transaction/notify');
+
+        $data = [];
         $result = [];
+        $priceData = [];
         foreach ($payment as $pay) {
             $price = Price::where('payment_id', $pay->payment_id)->where('game_id', $gameId)->orderBy('price', 'asc')->get();
+            if ($pay->channel_id == "GV") {
 
-            $p = array(
-                "payment_id" => $pay->payment_id,
-                "category_id" => $pay->category_id,
-                "country_id" => $pay->country_id,
-                "channel_id" => $pay->channel_id,
-                "name_channel" => $pay->name_channel,
-                "logo_channel" => url('/image/' . $pay->logo_channel),
-                "url"   => $pay->url,
-                "created_at" => $pay->created_at,
-                "updated_at" => $pay->updated_at
-            );
+                $p = array(
+                    "merchanId" => $Id_GV,
 
-            $data = [
-                'payment' => $p,
-                'price' => $price
-            ];
+                    "payment_id" => $pay->payment_id,
+                    // "category_id" => $pay->category_id,
+                    "country_id" => $pay->country_id,
+                    "channelId" => $pay->channel_id,
+                    "name_channel" => $pay->name_channel,
+                    "logo_channel" => url('/image/' . $pay->logo_channel),
+                    "url"   => $pay->url,
+                    "created_at" => $pay->created_at,
+                    "updated_at" => $pay->updated_at
+                );
+            } else if ($pay->channel_id == "ID_KLIKBCA") {
+                $p = array(
+                    "guid" => $Id_Unipin,
+                    "urlAck" => $notif_Unipin,
+
+                    "payment_id" => $pay->payment_id,
+                    // "category_id" => $pay->category_id,
+                    "country_id" => $pay->country_id,
+                    "channel" => $pay->channel_id,
+                    "name_channel" => $pay->name_channel,
+                    "logo_channel" => url('/image/' . $pay->logo_channel),
+                    "url"   => $pay->url,
+                    "created_at" => $pay->created_at,
+                    "updated_at" => $pay->updated_at
+                );
+            } else {
+                $p = array(
+                    "merchanId" => $Id_GOC,
+
+                    "payment_id" => $pay->payment_id,
+                    // "category_id" => $pay->category_id,
+                    "country_id" => $pay->country_id,
+                    "channelId" => $pay->channel_id,
+                    "name_channel" => $pay->name_channel,
+                    "logo_channel" => url('/image/' . $pay->logo_channel),
+                    "url"   => $pay->url,
+                    "created_at" => $pay->created_at,
+                    "updated_at" => $pay->updated_at
+                );
+            }
+
+
+            $data["payment"] = $p;
+            $data["price"] = $price;
 
             array_push($result, $data);
         };
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\api\PaymentController as ApiPaymentController;
 use App\Http\Controllers\api\TransactionController as ApiTransactionController;
 
 use App\Events\Transaction as EventsTransaction;
+use App\Models\Payment;
+use PhpParser\Node\Stmt\Echo_;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,11 +49,12 @@ Route::prefix('v1')->group(function () {
     });
 
 
-
+    // game list
     Route::get('/gamelist', [ApiGameController::class, 'index']);
     Route::get('/gamedetail', [ApiGameController::class, 'gameDetail']);
 
 
+    // payment
     Route::get('/payment', [ApiPaymentController::class, 'index']);
 
     Route::get('/allpayment', [ApiPaymentController::class, 'getAllPayment']);
