56d9162a163d38ea173ffd383c077af29ef7da17
gumelarid <galuhprahadi96@gmail.com>
2023-01-02 11:46:04 +0700

    update and fix all callback transaction

--- app/Http/Controllers/cms/TransactionController.php
+++ app/Http/Controllers/cms/TransactionController.php
@@ -20,6 +20,114 @@ class TransactionController extends Controller
 
     protected $data = array();
 
+
+    private function _goc($request)
+    {
+
+
+        if ($request->status == 100) {
+            $status = 1;
+            Log::info('Success Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Success Transaction Paid with GOC Invoice ' . $request->trxId]);
+        } else {
+
+            $status = 2;
+            Log::info('Cancel Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Cancel Transaction Paid with GOC Invoice ' . $request->trxId]);
+        };
+
+        $trx = Transaction::where('invoice', $request->trxId)->update([
+            'status' => $status
+        ]);
+
+        $detail = Transaction::where('invoice', $request->trxId)->first();
+
+        $price = Price::with('payment', 'pricepoint')->where('price_id', $detail->price_id)->first();
+
+        TransactionDetail::create([
+            'detail_id' => Str::uuid(),
+            'invoice_id' => $detail->invoice,
+            'player_id' => $detail->id_player,
+            'game_id' => $detail->game_id,
+            'ppi' => $price->pricepoint->price_point,
+            'method' => $price->payment->name_channel,
+            'amount' => $price->amount . ' ' . $price->name,
+            'total_paid' => $detail->total_price,
+            'paid_time' => $request->paidDate,
+        ]);
+    }
+
+    private function _unipin($request)
+    {
+
+        if ($request['transaction']['status'] == 0) {
+            $status = 1;
+            Log::info('Success Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Success Transaction Paid with GOC Invoice ' . $request['transaction']['reference']]);
+        } else {
+
+            $status = 2;
+            Log::info('Cancel Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Cancel Transaction Paid with GOC Invoice ' . $request['transaction']['reference']]);
+        };
+
+        $trx = Transaction::where('invoice', $request['transaction']['reference'])->update([
+            'status' => $status
+        ]);
+
+        $detail = Transaction::where('invoice', $request['transaction']['reference'])->first();
+
+        $price = Price::with('payment', 'pricepoint')->where('price_id', $detail->price_id)->first();
+
+        TransactionDetail::create([
+            'detail_id' => Str::uuid(),
+            'invoice_id' => $detail->invoice,
+            'player_id' => $detail->id_player,
+            'game_id' => $detail->game_id,
+            'ppi' => $price->pricepoint->price_point,
+            'method' => $price->payment->name_channel,
+            'amount' => $price->amount . ' ' . $price->name,
+            'total_paid' => $detail->total_price,
+            'paid_time' => date('d-m-Y H:i', $request['transaction']['time'])
+        ]);
+    }
+
+    private function _gudangVoucher($request)
+    {
+        $dataXML = $request->data;
+        $xmlObject = simplexml_load_string($dataXML);
+
+        $json = json_encode($xmlObject);
+        $phpArray = json_decode($json, true);
+
+        Log::info('info', ['data' => $phpArray]);
+        EventsTransaction::dispatch($phpArray['custom']);
+
+        if ($phpArray['status'] == "SUCCESS") {
+            $status = 1;
+            Log::info('Success Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Success Transaction Paid with GV Invoice ' . $phpArray['custom']]);
+        } else {
+            $status = 2;
+            Log::info('Cancel Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Cancel Transaction Paid with GV Invoice ' . $phpArray['custom']]);
+        };
+
+        $trx = Transaction::where('invoice', $phpArray['custom'])->update([
+            'status' => $status
+        ]);
+
+        $detail = Transaction::where('invoice', $phpArray['custom'])->first();
+
+        $price = Price::with('payment', 'pricepoint')->where('price_id', $detail->price_id)->first();
+
+        TransactionDetail::create([
+            'detail_id' => Str::uuid(),
+            'invoice_id' => $detail->invoice,
+            'player_id' => $detail->id_player,
+            'game_id' => $detail->game_id,
+            'ppi' => $price->pricepoint->price_point,
+            'method' => $price->payment->name_channel,
+            'amount' => $price->amount . ' ' . $price->name,
+            'total_paid' => $detail->total_price,
+            'paid_time' => date('d-m-Y H:i', $phpArray['payment_time']),
+        ]);
+    }
+
     public function index(Request $request)
     {
         try {
@@ -27,7 +135,7 @@ class TransactionController extends Controller
 
             $title = "Transaction History";
 
-            $data = Transaction::with('pricepoint', 'payment', 'game')->orderBy('created_at', 'desc')->get();
+            $data = Transaction::with('payment', 'game')->orderBy('created_at', 'desc')->get();
 
 
             return view('cms.pages.transaction.index', compact('title', 'data'));
@@ -44,13 +152,14 @@ class TransactionController extends Controller
     public function notify(Request $request)
     {
 
+        // dd($request->transaction['reference']);
         $trx = null;
         $status = null;
 
-        Log::critical('Critical error', $request->all());
-        Log::info('info', ['data' => $request->all()]);
-        Log::error('error', ['data' => $request->all()]);
-        Log::warning('warning', ['data' => $request->all()]);
+        // // Log::critical('Critical error', $request->all());
+        // // Log::info('info', ['data' => $request->all()]);
+        // // Log::error('error', ['data' => $request->all()]);
+        // // Log::warning('warning', ['data' => $request->all()]);
 
         DB::beginTransaction();
         try {
@@ -58,85 +167,31 @@ class TransactionController extends Controller
             if ($request->data) {
                 // GV
 
-                $dataXML = $request->data;
-                $xmlObject = simplexml_load_string($dataXML);
+                $this->_gudangVoucher($request);
+            } else if ($request->transaction) {
 
-                $json = json_encode($xmlObject);
-                $phpArray = json_decode($json, true);
-
-                Log::info('info', ['data' => $phpArray]);
-                // Log::error('error', ['data' => $phpArray['status']]);
-                // Log::warning('warning', ['data' => $phpArray['custom']]);
-                EventsTransaction::dispatch($phpArray['custom']);
-
-
-                if ($phpArray['status'] == "SUCCESS") {
-                    $status = 2;
-                    Log::info('Success Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Success Transaction Paid with GV Invoice ' . $phpArray['custom']]);
-                } else {
-                    $status = 1;
-                    Log::info('Cancel Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Cancel Transaction Paid with GV Invoice ' . $phpArray['custom']]);
-                };
-
-                $trx = Transaction::where('invoice', $phpArray['custom'])->update([
-                    'status' => $status
-                ]);
-
-                $detail = Transaction::where('invoice', $phpArray['custom'])->first();
-
-                $price = Price::with('payment', 'pricepoint')->where('price_id', $detail->price_id)->first();
-
-                TransactionDetail::create([
-                    'detail_id' => Str::uuid(),
-                    'invoice_id' => $detail->trxId,
-                    'player_id' => $detail->id_player,
-                    'game_id' => $detail->game_id,
-                    'ppi' => $price->pricepoint->price_point,
-                    'method' => $price->payment->name_channel,
-                    'amount' => $price->amount . ' ' . $price->name,
-                    'total_paid' => $detail->total_price,
-                    'paid_time' => date('d-m-Y H:i', $phpArray['payment_time']),
-                ]);
+                // Unipin ;
+                Log::info('info', ['data' => $request->all()]);
+                EventsTransaction::dispatch($request->transaction['reference']);
+                $this->_unipin($request);
             } else {
 
                 // GOC ;
-
                 Log::info('info', ['data' => $request->all()]);
                 EventsTransaction::dispatch($request->trxId);
-
-                if ($request->status == 100) {
-                    $status = 2;
-                    Log::info('Success Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Success Transaction Paid with GOC Invoice ' . $request->trxId]);
-                } else {
-
-                    $status = 0;
-                    Log::info('Cancel Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Cancel Transaction Paid with GOC Invoice ' . $request->trxId]);
-                };
-
-                $trx = Transaction::where('invoice', $request->trxId)->update([
-                    'status' => $status
-                ]);
-
-                $detail = Transaction::where('invoice', $request->trxId)->first();
-
-                $price = Price::with('payment', 'pricepoint')->where('price_id', $detail->price_id)->first();
-
-                TransactionDetail::create([
-                    'detail_id' => Str::uuid(),
-                    'invoice_id' => $detail->trxId,
-                    'player_id' => $detail->id_player,
-                    'game_id' => $detail->game_id,
-                    'ppi' => $price->pricepoint->price_point,
-                    'method' => $price->payment->name_channel,
-                    'amount' => $price->amount . ' ' . $price->name,
-                    'total_paid' => $detail->total_price,
-                    'paid_time' => $request->paidDate,
-                ]);
+                $this->_goc($request);
             };
 
             DB::commit();
 
-            return "OK";
+            if ($request->transaction) {
+                return \response()->json([
+                    'status' => 0,
+                    'message' => 'Reload Successful',
+                ], Response::HTTP_OK);
+            } else {
+                return "OK";
+            };
         } catch (\Throwable $th) {
             DB::rollback();
             Log::error('Error Notify TopUp Transaction ', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | ERR ' . ' | Error Notify TopUp Transaction']);
