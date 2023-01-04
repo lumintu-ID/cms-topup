134beb33790bd558252a62ad0c9710984ee8ec97
Rijal Ahmad Taufik <rijalahmadt@gmail.com>
2023-01-04 15:12:00 +0700

    Add games controller

---
+++ app/Http/Controllers/frontend/GameController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    public function index()
+    {
+        return 'echo payment';
+    }
+}
