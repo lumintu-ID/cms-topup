0951c2f2f26040385d0f3e65224b84642640cd63
gumelarid <galuhprahadi96@gmail.com>
2023-03-07 10:30:10 +0700

    update

--- app/Helpers/GOC.php
+++ app/Helpers/GOC.php
@@ -58,11 +58,37 @@ class Goc
             'sign' => $signature
         ]);
 
-        var_dump($response->body());
-        // return response()->json([
-        //     'code' => 200,
-        //     'status' => 'SUCCESS',
-        //     'data' => $response->body(),
-        // ], 200);
+        $result = $response->body();
+
+        dd($result);
+
+        // if ($result->status == 100) {
+        //     $status = 1;
+        //     Log::info('Success Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Success Transaction Paid with GOC Invoice ' . $request->trxId]);
+        // } else {
+
+        //     $status = 2;
+        //     Log::info('Cancel Transaction Paid', ['DATA' => Carbon::now()->format('Y-m-d H:i:s') . ' | INFO ' . ' | Cancel Transaction Paid with GOC Invoice ' . $request->trxId]);
+        // };
+
+        // $trx = Transaction::where('invoice', $result->trxId)->update([
+        //     'status' => $status
+        // ]);
+
+        // $detail = Transaction::where('invoice', $result->trxId)->first();
+
+        // $price = Price::with('payment', 'pricepoint')->where('price_id', $detail->price_id)->first();
+
+        // TransactionDetail::create([
+        //     'detail_id' => Str::uuid(),
+        //     'invoice_id' => $detail->invoice,
+        //     'player_id' => $detail->id_Player,
+        //     'game_id' => $detail->game_id,
+        //     'ppi' => $price->pricepoint->price_point,
+        //     'method' => $price->payment->name_channel,
+        //     'amount' => $price->amount . ' ' . $price->name,
+        //     'total_paid' => $detail->total_price,
+        //     'paid_time' => $result->paidDate,
+        // ]);
     }
 }
