5c67793ae8636b3e6adb5897d15691e318f6abc6
gumelarid <galuhprahadi96@gmail.com>
2023-04-12 16:45:44 +0700

    fix notification

--- app/Helpers/GudangVoucher.php
+++ app/Helpers/GudangVoucher.php
@@ -7,6 +7,7 @@ use App\Models\Transaction;
 use Illuminate\Support\Str;
 use Illuminate\Support\Carbon;
 use App\Models\TransactionDetail;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 
